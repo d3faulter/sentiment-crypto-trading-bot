34083f21e26f445cda02f7b1c203eb0541dc04fa
d3faulter <nvfbeck@gmail.com>
2023-08-10 22:17:59 +0200

    fixes

--- Bot1/index.js
+++ Bot1/index.js
@@ -1,15 +1,22 @@
 const axios = require('axios');
 const { RestClientV5 } = require('bybit-api');
+require('dotenv').config({ path: '../.env' });
 
-// Timer til at køre
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
+const client = new RestClientV5({
+    apiKey: process.env.BYBIT_API_KEY, 
+    apiSecret: process.env.BYBIT_API_SECRET,
+});
+
+
+// // Timer til at køre
+ module.exports = async function (context, myTimer) {
+     var timeStamp = new Date().toISOString();
     
-    if (myTimer.isPastDue)
-    {
+     if (myTimer.isPastDue)
+     {
         context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+     }
+     context.log('JavaScript timer trigger function ran!', timeStamp);   
    
     const news = await fetchNews();
     const coinSymbolsFromNews = extractCoinSymbolsFromNews(news);
@@ -21,27 +28,28 @@ module.exports = async function (context, myTimer) {
 
     const coinData = await fetch24HourData(commonCoins);
     context.log(coinData);
-};
+ };
 
 
 // Fetch news
 async function fetchNews() {
     const allNews = [];
-    
+
     // Response is paginated, so we need to loop through all pages
     try {
         for (let page = 1; page <= 5; page++) {
             const response = await axios.get('https://cryptopanic.com/api/v1/posts/', {
                 params: {
-                    auth_token: '',
+                    auth_token: process.env.CRYPTOPANIC_API_KEY,
                     kind: 'news',
                     page: page
                 },
             });
             allNews.push(...response.data.results);
         }
-        
+        console.log(`Fetched: ${allNews.length} articles`);
         return allNews;
+
     } catch (error) {
         console.error(`Failed to fetch news: ${error}`);
         return null;
@@ -49,19 +57,27 @@ async function fetchNews() {
 }
 
 // Save coins from news response
-function extractCoinSymbolsFromNews(news) {
+function extractCoinSymbolsFromNews(News) { // Should be allNews?
     const coinSymbols = [];
 
-    for (const article of news) {
-        for (const currency of article.currencies) {
-            if (!coinSymbols.includes(currency.code)) {
-                coinSymbols.push(currency.code);
+    try {
+        for (const article of News) {
+            // Check if article.currencies is defined and is an array
+            if (Array.isArray(article.currencies)) {
+                for (const currency of article.currencies) {
+                    if (!coinSymbols.includes(currency.code))  {
+                        coinSymbols.push(currency.code);
+                    }
+                }
             }
         }
+        console.log(`Extracted ${coinSymbols.length} coins from news articles`);
+        return coinSymbols;
+    } catch (error) {
+        console.error(`Failed to extract coins from news: ${error}`);
     }
-
-    return coinSymbols;
 }
+
 
 // Check if coins in coinSymbols are available on ByBit and return the available ones
 async function fetchAvailableCoins(coinSymbols) {
@@ -72,7 +88,8 @@ async function fetchAvailableCoins(coinSymbols) {
             category: 'spot',
         });
 
-        if (response.retCode === 0) {
+        console.log(response);
+*        if (response.retCode === 0) {
             const tickers = response.result.list;
 
             for (const ticker of tickers) {
@@ -86,28 +103,102 @@ async function fetchAvailableCoins(coinSymbols) {
         console.error(`Failed to fetch available coins: ${error}`);
     }
 
+    console.log(`Found ${availableCoins.length} coins available on ByBit`);
     return availableCoins;
 }
 
-// Fetch 24 hour ticker data (should only be done if coin is assessed as either buy or sell)
-// Currently checks all coins, but should only check the ones that are assessed as either buy or sell
-async function fetch24HourData(availableCoins) {
-    const data = {};
+// ### Sentiment analysis of news ###
 
-    for (const symbol of availableCoins) {
+// Match availableCoins with fetched news to get coinNews, which is an array of the news that are relevant for the available coins
+function matchNewsWithCoins(availableCoins, allNews) {
+    const coinNews = [];
+
+    for (const article of allNews) {
+        for (const currency of article.currencies) {
+            if (availableCoins.includes(currency.code)) {
+                coinNews.push(article);
+            }
+        }
+    }
+    console.log(coinNews);
+    return coinNews;
+
+}
+
+// Sentiment analysis of news using OpenAI
+async function analyzeSentiment(coinNews) {
+    const sentiments = {};
+
+    for (const article of coinNews) {
         try {
-            const response = await client.getTickers({
-                category: 'spot',
-                symbol: symbol,
+            const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+                prompt: `Analyze the sentiment of this headline: "${article.title}". Rate it from 0 (very negative) to 10 (very positive).`,
+                max_tokens: 5
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                    'Content-Type': 'application/json'
+                }
             });
 
-            if (response.retCode === 0) {
-                data[symbol] = response.result;
+            const score = parseFloat(response.data.choices[0].text.trim());
+            if (score >= 8) {
+                sentiments[article.currency.code] = 'buy';
+            } else if (score <= 2) {
+                sentiments[article.currency.code] = 'sell';
+            } else {
+                sentiments[article.currency.code] = 'neutral';
             }
         } catch (error) {
-            console.error(`Failed to fetch 24-hour data for ${symbol}: ${error}`);
+            console.error(`Failed to analyze sentiment for ${article.title}: ${error}`);
+        }
+    }
+
+    return sentiments;
+}
+
+
+// Fetch 24 hour ticker data (should only be done if coin is assessed as either buy or sell)
+// Currently checks all coins, but should only check the ones that are assessed as either buy or sell -> Change availableCoins to res of above analysis
+async function fetch24HourData(sentiments) {
+    const data = {};
+
+    for (const symbol in sentiments) {
+        if (sentiments[symbol] === 'buy' || sentiments[symbol] === 'sell') {
+            try {
+                const response = await client.getTickers({
+                    category: 'spot',
+                    symbol: symbol,
+                });
+
+                if (response.retCode === 0) {
+                    data[symbol] = response.result;
+                }
+            } catch (error) {
+                console.error(`Failed to fetch 24-hour data for ${symbol}: ${error}`);
+            }
         }
     }
 
     return data;
 }
+
+
+// Price analysis with EMA's, MACD, RSI and Volume. Evaluated by GPT
+
+
+
+// !!!!!!!!!!!!!!!!!! Add all new functons to timer trigger in the top !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+// Mock context and myTimer objects for local testing
+const mockContext = {
+    log: console.log
+};
+
+const mockTimer = {
+    isPastDue: false
+};
+
+// Call the main function directly
+module.exports(mockContext, mockTimer);
