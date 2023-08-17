45a0af315af480aefc795652c2f0924650ba63c1
d3faulter <nvfbeck@gmail.com>
2023-08-17 11:33:31 +0200

    Added rough technical analysis

--- Bot1/index.js
+++ Bot1/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const ti = require('technicalindicators');
 const { RestClientV5 } = require('bybit-api');
 require('dotenv').config({ path: '../.env' });
 
@@ -49,24 +50,45 @@ module.exports = async function (context, myTimer) {
         const availableCoins = await fetchAvailableCoins();
         const relevantNews = getRelevantCoinNews(coinSymbolsFromNews, availableCoins, news);
        
-        // context.log(coinSymbolsFromNews); // Very long terminal output if uncommented
-       // context.log(relevantNews);
-
        const allRelevantNews = [].concat(...Object.values(relevantNews));
-       const sentiments = await analyzeSentiment(allRelevantNews);
-    //    console.log(sentiments);
+        
+        allRelevantNews.forEach(article => {
+             context.log(article.title + ' ' + article.domain);
+        });
+
+        const sentiments = await analyzeSentiment(allRelevantNews);
         context.log(sentiments);
+
+//       const sentiments = {
+//        'BTC': 'strong buy',
+//        'ETH': 'buy',
+//        'ADA': 'sell',
+//        'XRP': 'strong sell',
+//        'DOT': 'strong buy'
+//    };
+
+      const coinData = await fetch7DayKlineData(sentiments);
+        context.log(coinData);
+
+        // Calculate technical indicators and assign an overall score for each coin
+        const coinScores = {};
+        for (const symbol in coinData) {
+            coinScores[symbol] = calculateTechnicalIndicators(coinData[symbol]);
+        }
+        context.log(coinScores);
 
     } catch (error) {
         context.log('Error in main function:', error.message);
     }
 };
 
+// Fetcher from Cryptopanic
+
 async function fetchNews() {
     const allNews = [];
     const baseURL = 'https://cryptopanic.com/api/v1/posts/';
 
-    for (let page = 1; page <= 1; page++) { // remember to change loop back to page 5
+    for (let page = 1; page <= 4; page++) { // remember to change loop back to page 5
         try {
             const response = await axios.get(baseURL, {
                 params: {
@@ -125,21 +147,30 @@ async function fetchAvailableCoins() {
 
 function getRelevantCoinNews(coinSymbolsFromNews, availableCoins, allNews) {
     const relevantCoinNews = {};
+    const uniqueArticles = new Set(); // To store unique articles
 
     coinSymbolsFromNews.forEach(symbol => {
         if (availableCoins.includes(symbol)) {
-            relevantCoinNews[symbol] = allNews.filter(article => {
+            const filteredArticles = allNews.filter(article => {
                 return Array.isArray(article.currencies) && article.currencies.some(currency => currency.code === symbol);
             });
+            
+            // Add unique articles to the Set
+            filteredArticles.forEach(article => uniqueArticles.add(JSON.stringify(article)));
         }
     });
 
-    console.log(`Found ${Object.keys(relevantCoinNews).length} matched coins between news and ByBit. Analyzing sentiment of ${allNews.length} headlines...`);
-    return relevantCoinNews;
+    // Convert the Set back to an array
+    const uniqueArticleArray = Array.from(uniqueArticles).map(articleStr => JSON.parse(articleStr));
+
+    console.log(`Found ${uniqueArticleArray.length} unique matched articles between news and ByBit. Analyzing sentiment of headlines: \n`);
+    return uniqueArticleArray;
 }
+
 
 // Sentiment analysis of news using OpenAI's Davinci model
 
+// Progress bar to track sentiment analysis
 function printProgressBar(percentage) {
     const length = 40; // Length of the progress bar
     const position = Math.floor((percentage / 100) * length);
@@ -147,7 +178,7 @@ function printProgressBar(percentage) {
     for (let i = 0; i < position; i++) {
         progressBar[i] = '=';
     }
-    process.stdout.clearLine();  // Clear the current line in terminal
+    process.stdout.clearLine();
     process.stdout.cursorTo(0);  // Move cursor to the beginning of the line
     process.stdout.write(`[${progressBar.join('')}] ${percentage.toFixed(2)}%`); // Print the progress bar
 }
@@ -159,13 +190,14 @@ async function analyzeSentiment(coinNews) {
     const totalArticles = coinNews.length;
     let processedArticles = 0;
 
+    console.log('\n'); 
     printProgressBar(0); // Initial progress bar
 
     for (const article of coinNews) {
         try {
             // 1. Extract Sentiment from the Headline
             const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-                prompt: `You are a master at sentiment analysis of news in relation to the stock and crypto market. You know exactly how a stock or coin will move due to the impact of a news article. Please analyze the sentiment of this headline: "${article.title}". Rate it from 0 (very negative) to 10 (very positive).`,
+                prompt: `You are a specialized AI expert in sentiment analysis, with a deep understanding of nuances and subtleties in news related to the stock and cryptocurrency markets. Each headline may contain overt signals or subtle hints about its sentiment. Your analysis directly influences trading decisions, making accuracy and precision absolutely essential. Please read and evaluate the following headline multiple times, considering both its overt and subtle cues, its relevance to the specific cryptocurrency, and its potential impact on market movements: "${article.title}". Provide a sentiment score based on the content of "${article.title}" from 0 (extremely negative) to 10 (extremely positive). Only if the headline is ambiguous or its impact is truly uncertain, only then rate it as neutral. Ensure you've considered every aspect of the headline before finalizing your score.`,
                 max_tokens: 5
             }, {
                 headers: {
@@ -213,38 +245,92 @@ async function analyzeSentiment(coinNews) {
                            averageScore >= 8  ? 'buy' :
                            averageScore <= 3  ? 'strong sell' :
                            averageScore <= 5  ? 'sell' : 'neutral';
-                           console.log(sentiments[coin]);
     }
 
     return sentiments;
 }
 
-// Fetch 24 hour ticker data (should only be done if coin is assessed as either buy or sell)
-// Currently checks all coins, but should only check the ones that are assessed as either buy or sell -> Change availableCoins to res of above analysis
-async function fetch24HourData(sentiments) {
+// Fetch 24 hour ticker data for all non-neutral coins
+
+async function fetch7DayKlineData(sentiments) {
     const data = {};
 
-    for (const symbol in sentiments) {
-        if (sentiments[symbol] === 'buy' || sentiments[symbol] === 'sell') {
-            try {
-                const response = await client.getTickers({
-                    category: 'spot',
-                    symbol: symbol,
-                });
+    // Filter coins based on their sentiment
+    const relevantCoins = Object.keys(sentiments).filter(symbol => 
+        ['buy', 'strong buy', 'sell', 'strong sell'].includes(sentiments[symbol])
+    );
 
-                if (response.retCode === 0) {
-                    data[symbol] = response.result;
-                }
-            } catch (error) {
-                console.error(`Failed to fetch 24-hour data for ${symbol}: ${error}`);
+    const oneWeekAgo = Date.now() - (7 * 24 * 60 * 60 * 1000); // 7 days in milliseconds
+
+    for (const symbol of relevantCoins) {
+        try {
+            const response = await client.get('/v5/market/kline', {
+                category: 'spot',
+                symbol: symbol + 'USDT', // Fetching USDT pairs
+                interval: '60',
+                start: oneWeekAgo,
+                end: Date.now()
+            });
+
+            if (response.retCode === 0 && response.result && response.result.list) {
+                data[symbol] = response.result.list; // This will be an array of Kline data for the past 7 days
             }
+        } catch (error) {
+            console.error(`Failed to fetch 7-day Kline data for ${symbol}: ${error.message}`);
         }
     }
 
     return data;
 }
 
-// Price analysis with EMA's, MACD, RSI and Volume. Evaluated by GPT
+
+// Technical analysis
+
+function calculateTechnicalIndicators(klineData) {
+    const close = klineData.map(data => parseFloat(data[4]));
+    const high = klineData.map(data => parseFloat(data[2]));
+    const low = klineData.map(data => parseFloat(data[3]));
+    const volume = klineData.map(data => parseFloat(data[5]));
+
+    // Calculate RSI
+    const rsi = ti.RSI.calculate({ values: close, period: 14 });
+    const currentRSI = rsi[rsi.length - 1];
+
+    // Calculate MACD
+    const macdResult = ti.MACD.calculate({
+        values: close,
+        fastPeriod: 12,
+        slowPeriod: 26,
+        signalPeriod: 9,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false
+    });
+    const currentMACD = macdResult[macdResult.length - 1];
+
+    // Calculate EMA
+    const ema50 = ti.EMA.calculate({ values: close, period: 50 });
+    const currentEMA50 = ema50[ema50.length - 1];
+    const ema200 = ti.EMA.calculate({ values: close, period: 200 });
+    const currentEMA200 = ema200[ema200.length - 1];
+
+    // Calculate Bollinger Bands
+    const bb = ti.BollingerBands.calculate({ values: close, period: 20, stdDev: 2 });
+    const currentBB = bb[bb.length - 1];
+
+    // Calculate Stochastic Oscillator
+    const stochastic = ti.Stochastic.calculate({
+        high: high,
+        low: low,
+        close: close,
+        period: 14,
+        signalPeriod: 3
+    });
+    const currentStochastic = stochastic[stochastic.length - 1];
+
+    // Here, you can add logic to evaluate the calculated indicators and return an overall score.
+    // For simplicity, I'll return a random score between 1 and 10.
+    return Math.floor(Math.random() * 10) + 1;
+}
 
 // Mock context and myTimer objects for local testing
 const mockContext = {
@@ -254,6 +340,8 @@ const mockContext = {
 const mockTimer = {
     isPastDue: false
 };
+
+
 
 // Call the main function directly
 module.exports(mockContext, mockTimer);
